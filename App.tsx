5b3865bde10bca6636a5c997c518877d5b96895b
Sanket V <sanketvithani6@gmail.com>
2023-12-26 17:08:16 +0530

    => Integrate env in android

--- App.tsx
+++ App.tsx
@@ -1,14 +1,28 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import Config from 'react-native-config';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 
 const App = () => {
+  console.log('Config----------', Config);
   return (
-    <View>
-      <Text>App</Text>
+    <View style={styles.container}>
+      <SafeAreaView />
+      <Text style={styles.envText}>{Config.ENV}</Text>
     </View>
   );
 };
 
 export default App;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: 'white',
+    justifyContent: 'center',
+  },
+  envText: {
+    fontSize: 20,
+    color: 'black',
+  },
+});
